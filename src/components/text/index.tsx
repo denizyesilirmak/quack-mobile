235d66ec093fe84b6c5a9ce5058790d4ad9d5ea0
denizyesilirmak <denizyesilirmak@gmail.com>
2023-10-30 22:30:15 +0300

    Add Text Component

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,8 +1,26 @@
-import {Text as RNText} from 'react-native';
-import {TextProps} from './types';
+import { Text as RNText } from 'react-native';
+import { TextProps } from './types';
 
-const Text = ({children}: TextProps) => {
-  return <RNText>{children}</RNText>;
+const textSizes = {
+  xs: 12,
+  sm: 14,
+  md: 18,
+  lg: 20,
+  xl: 24,
+};
+
+const Text = ({ children, size, style, color }: TextProps) => {
+  return (
+    <RNText
+      style={{
+        fontFamily: 'VarelaRound-Regular',
+        fontSize: textSizes[size || 'sm'],
+        ...(style as object),
+        color: color || '#000000',
+      }}>
+      {children}
+    </RNText>
+  );
 };
 
 export default Text;
--- src/components/text/types.ts
+++ src/components/text/types.ts
@@ -1,5 +1,10 @@
+import { StyleProp, TextStyle } from 'react-native';
+
 type TextProps = {
   children: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  style?: StyleProp<TextStyle>;
+  color?: string;
 };
 
-export type {TextProps};
+export type { TextProps };
